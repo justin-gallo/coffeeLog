917d0612b07b31e3a73eeed623fb668a02f63aef
Justin Gallo <contact@justingallo.dev>
2022-03-03 10:43:23 -0500

    Add modal & respective CSS/JS

--- coffee.js
+++ coffee.js
@@ -1,5 +1,6 @@
 let myLog = [];
 
+//Create a new coffee object
 function Coffee(name, roaster, flavorProfile, goodOrBad) {
     this.name = name
     this.roaster = roaster
@@ -7,12 +8,31 @@ function Coffee(name, roaster, flavorProfile, goodOrBad) {
     this.goodOrBad = goodOrBad
 }
 
-
-
+//get info about Coffee object
 Coffee.prototype.info = function() {
     if (this.goodOrBad === "goodCoffee") {
         return `${this.name} by ${this.roaster}. Flavor profile: ${this.flavorProfile}. I think it's a good coffee.`;
     } else if (this.goodOrBad === "badCoffee") {
         return `${this.name} by ${this.roaster}. Flavor profile: ${this.flavorProfile}. I think it's a bad coffee.`;
+    }
+}
+
+//Modal Script://
+const modal = document.getElementById("newCoffeeModal"); //select the modal popup
+const addCoffee = document.getElementById("addCoffee"); //select the "+ add coffee" btn
+const closeBtn = document.getElementsByClassName("closeButton")[0]; //select the modal's close btn
+
+//Display the modal when the "+ add coffee" button is clicked
+addCoffee.onclick = function() {
+    modal.style.display = "block";
+}
+//Close the modal when the "x" is clicked
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+//Close the modal when the window outside of the modal is clicked
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
     }
 }
